14051234c2c4f731d50f43236d6ebf168d0dd3c7
bolanebyla <alexleskov2000@gmail.com>
2020-03-17 23:54:39 +0800

    Добавил вид основных функций

--- DB.py
+++ DB.py
@@ -5,7 +5,8 @@ def get_institute():
 
 
 # Список курсов у определённого института
-def get_course(inst_id=0):
+def get_course(inst_id=0, inst_name=''):
+    # (данные беруться из базы либо по id либо по названию института)
     courses_list = [{'name': '1 курс', 'course_id': 1}, {'name': '2 курс', 'course_id': 2}]
     return courses_list
 
@@ -14,6 +15,14 @@ def get_course(inst_id=0):
 def get_group(cours_id=0):
     groups_list = [{'name': 'ИБб-18-1', 'group_id': 1}, {'name': 'ИБб-18-2', 'group_id': 2}]
     return groups_list
+
+
+def get_user_info(chat_id=0):
+    # (Заполнять словами)
+    user_info = {'chat_id': chat_id, 'inst': 'ИИТиАД', 'course': '',
+                 'group': ''}  # Если в базе данных какое-то поле пустое, то записать пустую строку или None
+
+    return user_info
 
 
 # Записываем в базу институт пользователя
@@ -28,4 +37,19 @@ def set_user_course(chat_id=0, course_id=0):
 
 # Записываем в базу группу пользователя
 def set_user_group(chat_id=0, group_id=0):
+    pass
+
+
+# Удаляем информацию об институте пользователя из базы данных
+def del_user_inst(chat_id=0):
+    pass
+
+
+# Удаляем информацию о курсе пользователя из базы данных
+def del_user_course(chat_id=0):
+    pass
+
+
+# Удаляем информацию о пользователе из базы данных
+def del_user_info(chat_id=0):
     pass
